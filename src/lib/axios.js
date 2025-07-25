9a9bc0705fe3a883af9b5176108677ffd827dce6
Mayanksinghhh <128398164+Mayanksinghhh@users.noreply.github.com>
2025-07-25 16:06:18 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const baseURL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:3000/api" // Local backend for dev
-    : "https://chatterbox-backend-wqxi.onrender.com/api"; // Render backend for production
+// const baseURL =
+//   import.meta.env.MODE === "development"
+//     ? "http://localhost:3000/api" // Local backend for dev
+//     : "https://chatterbox-backend-wqxi.onrender.com/api"; // Render backend for production
 
 export const axiosInstance = axios.create({
-  baseURL,
+  baseURL:"https://chatterbox-backend-jlmr.onrender.com/api",
   withCredentials: true,
 });
